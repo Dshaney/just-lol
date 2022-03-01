edf8e01bdd0310aacfaa4f2f457157e0cf80eec5
Dshane Godbold <dshanegodbold@gmail.com>
2022-03-01 14:17:09 -0600

    updated

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import got from 'got';
 
+
 async function main(){
-    const responce = await got("httpbin.org/get")
-    const data = responce.body;
-   console.log(data)
-}
+    const responce = await got("https://api.github.com/users/dshaney/repos")
+    const data = await got('https://api.github.com/users/dshaney/repos').json()
+    console.log(data)
+  }
+
+
 main()
