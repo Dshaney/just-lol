46e7de7fd0767bbdf383427e93e1af65cf4a8603
Dshane Godbold <dshanegodbold@gmail.com>
2022-03-01 13:40:25 -0600

    http update

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
 import got from 'got';
-import http from 'http';
+
 async function main(){
     const responce = await got("httpbin.org/get")
     const data = responce.body;
-    const options = {
-        method: 'get',
-        hostname: 'httpbin.org/get',
-        path: '/',
-    };
-    http.request(options, (res) =>{
-        let data = '';
-        res.on('data', (chunk) =>{
-            data += chunk.toString();
-        });
-        res.on('end', () =>{
-            console.log('All done !');
-            console.log(`Here is the data ${data}`)
-        })
-    })
+   console.log('')
 }
-
 main()
